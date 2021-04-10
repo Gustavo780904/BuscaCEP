48610ce69dd502f0c75e031b59853fef3065944c
Gustavo780904 <gustavodutrasantos@gmail.com>
2021-04-10 19:29:10 -0300

    busca cep não influencia

--- js/functions.js
+++ js/functions.js
@@ -1,78 +1,78 @@
-// $(document).ready(function () {
+$(document).ready(function () {
 
-//     function limpa_formulário_cep() {
-//         // Limpa valores do formulário de cep.
-//         $("#rua").val("");
-//         $("#bairro").val("");
-//         $("#cidade").val("");
-//         $("#uf").val("");
-//         $("#ibge").val("");
-//     }
+    function limpa_formulário_cep() {
+        // Limpa valores do formulário de cep.
+        $("#rua").val("");
+        $("#bairro").val("");
+        $("#cidade").val("");
+        $("#uf").val("");
+        $("#ibge").val("");
+    }
 
-//     //Quando o campo cep perde o foco.
-//     $("#cep").blur(function () {
+    //Quando o campo cep perde o foco.
+    $("#cep").blur(function () {
 
-//         //Nova variável "cep" somente com dígitos.
-//         var cep = $(this).val().replace(/\D/g, '');
+        //Nova variável "cep" somente com dígitos.
+        var cep = $(this).val().replace(/\D/g, '');
 
-//         //Verifica se campo cep possui valor informado.
-//         if (cep != "") {
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
 
-//             //Expressão regular para validar o CEP.
-//             var validacep = /^[0-9]{8}$/;
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
 
-//             //Valida o formato do CEP.
-//             if (validacep.test(cep)) {
+            //Valida o formato do CEP.
+            if (validacep.test(cep)) {
 
-//                 //Preenche os campos com "..." enquanto consulta webservice.
-//                 $("#rua").val("...");
-//                 $("#bairro").val("...");
-//                 $("#cidade").val("...");
-//                 $("#uf").val("...");
-//                 $("#ibge").val("...");
+                //Preenche os campos com "..." enquanto consulta webservice.
+                $("#rua").val("...");
+                $("#bairro").val("...");
+                $("#cidade").val("...");
+                $("#uf").val("...");
+                $("#ibge").val("...");
 
-//                 //Consulta o webservice viacep.com.br/
-//                 let cliente = $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
-//                     enderecoCliente = cliente.responseJSON
-//                     console.log(enderecoCliente);
-//                     //leitura do nome para add ao json
+                //Consulta o webservice viacep.com.br/
+                let cliente = $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
+                    enderecoCliente = cliente.responseJSON
+                    console.log(enderecoCliente);
+                    //leitura do nome para add ao json
 
-//                     if (!("erro" in dados)) {
-//                         //Atualiza os campos com os valores da consulta.
-//                         $("#rua").val(dados.logradouro);
-//                         $("#bairro").val(dados.bairro);
-//                         $("#cidade").val(dados.localidade);
-//                         $("#uf").val(dados.uf);
-//                         $("#ibge").val(dados.ibge);
+                    if (!("erro" in dados)) {
+                        //Atualiza os campos com os valores da consulta.
+                        $("#rua").val(dados.logradouro);
+                        $("#bairro").val(dados.bairro);
+                        $("#cidade").val(dados.localidade);
+                        $("#uf").val(dados.uf);
+                        $("#ibge").val(dados.ibge);
 
 
-//                     } //end if.
-//                     else {
-//                         //CEP pesquisado não foi encontrado.
-//                         limpa_formulário_cep();
-//                         alert("CEP não encontrado.");
-//                     }
-//                 });
-//             } //end if.
-//             else {
-//                 //cep é inválido.
-//                 limpa_formulário_cep();
-//                 alert("Formato de CEP inválido.");
-//             }
-//         } //end if.
-//         else {
-//             //cep sem valor, limpa formulário.
-//             limpa_formulário_cep();
-//         }
-//     });
+                    } //end if.
+                    else {
+                        //CEP pesquisado não foi encontrado.
+                        limpa_formulário_cep();
+                        alert("CEP não encontrado.");
+                    }
+                });
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    });
     
-// });
-// function addNome() {
-//     nome = document.getElementById("nomeCliente").value
-//     enderecoCliente.nome = document.getElementById("nomeCliente").value
-//     console.log(enderecoCliente);
-//     localStorage.setItem(document.getElementById("nomeCliente").value, JSON.stringify(enderecoCliente));
-// }
+});
+function addNome() {
+    nome = document.getElementById("nomeCliente").value
+    enderecoCliente.nome = document.getElementById("nomeCliente").value
+    console.log(enderecoCliente);
+    localStorage.setItem(document.getElementById("nomeCliente").value, JSON.stringify(enderecoCliente));
+}
 
 function mostrarClientes() {
     var arquivoJson = "https://github.com/Gustavo780904/BuscaCEP/blob/master/json/clientes.json"
